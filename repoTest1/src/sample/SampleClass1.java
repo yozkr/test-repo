df95af9d60341c301dd4bc99871a69ce17f88523
yohsuga <yosukes25@gmail.com>
2011-01-12 17:25:49 +0900

    initial

---
+++ repoTest1/src/sample/SampleClass1.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class SampleClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ repoTest2/src/example/TestClass2.java
@@ -0,0 +1,13 @@
+package example;
+
+public class TestClass2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
